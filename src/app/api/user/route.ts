209e6c2591cdaf398a25ecf06163d66348353d63
Akash <chakash793@gmail.com>
2025-07-18 17:21:17 +0530

    http methods

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,23 @@
+export function GET(){
+
+    return Response.json({
+        email:"mehbubowork@gmail.com",
+        name:"Mehbub"
+    })
+}
+
+export function POST(){
+
+    return Response.json({
+        email:"mehbubowork@gmail.com",
+        name:"Mehbub"
+    })
+}
+
+export function PUT(){
+
+    return Response.json({
+        email:"mehbubowork@gmail.com",
+        name:"Mehbub"
+    })
+}
