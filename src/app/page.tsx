728eb60ebb55f424846da6276f976430054eada6
Akash <chakash793@gmail.com>
2025-07-18 15:18:44 +0530

    Added Loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-import Image from "next/image";
+import axios from 'axios';
 
-export default function Home() {
-  return (
-    <div>Hi Akash</div>
-  );
+async function getUserData(){
+  await new Promise((r)=>setTimeout(r,3000))
+  const response = await axios.get('https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details')
+   return response.data;
+}
+
+export default async function Home(){
+  const userDetails =  await getUserData();
+
+  return(
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <div className="flex justify-center items-center">
+      <div className='border p-8 rounded'>
+        <div>
+        Name: {userDetails?.name}
+        </div>
+        {userDetails?.email}
+      </div>
+      </div>
+    </div>
+
+  )
 }
